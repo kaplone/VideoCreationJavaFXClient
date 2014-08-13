76471834022ac944c81e9ff9dd5199e786ad603c
david-NFA035 <david.bertrand@sudest.pleiad.net>
2014-08-13 16:35:00 +0200

    repères touch

--- VideoCreationJavaFXClient/src/main/java/applicationFX/Main.java
+++ VideoCreationJavaFXClient/src/main/java/applicationFX/Main.java
@@ -6,32 +6,19 @@ import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application implements javafx.fxml.Initializable {
 
-	@FXML
-	ImageView view_0;
-	
-	@FXML
-	Slider cursorFrame1;
-	
-	@FXML
-	Label frameNumber;
-	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("VCGUI.fxml"));
+			BorderPane root = FXMLLoader.load(getClass().getResource("VCGUI.fxml"));
 			Scene scene = new Scene(root,1000,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -48,9 +35,6 @@ public class Main extends Application implements javafx.fxml.Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		System.out.println("init");
-		
-		 view_0.setImage(new Image("file:///home/autor/git/VideoCreationJavaFXClient/VideoCreationJavaFXClient/images/frames_WakeApp/image2_00042.png"));
 	}
 
 }
--- VideoCreationJavaFXClient/src/main/java/applicationFX/VCGUIController.java
+++ VideoCreationJavaFXClient/src/main/java/applicationFX/VCGUIController.java
@@ -1,15 +1,25 @@
 package applicationFX;
 
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
-public class VCGUIController {
+
+public class VCGUIController implements Initializable{
 	
 	
 	@FXML
@@ -18,6 +28,8 @@ public class VCGUIController {
 	private Slider cursorFrame1;
 	@FXML
 	private ImageView view_0;
+    @FXML
+    private BorderPane root;
 	@FXML
 	private Button play;
 	@FXML
@@ -26,8 +38,14 @@ public class VCGUIController {
 	private double f;
 	private Image nouvelle ;
 	boolean run = true;
+	
+	Line verticale = new Line();
+	Line horizontale = new Line();
+	Line [] lines = new Line [] {verticale, horizontale};
 
 	ViewService viewService = new ViewService(this);
+	
+	boolean added = false;
 	
 	
 	@FXML
@@ -65,6 +83,52 @@ public class VCGUIController {
 	 protected void OnStopClicked() {
 		run = false;
 	    }
+	
+	@FXML
+	 protected void OnMouseOverFrame() {
+		
+		double offsetX = view_0.getLayoutX();
+		double offsetY = view_0.getLayoutY();
+		
+		horizontale.setStartX(offsetX);
+		horizontale.setEndX(250 + offsetX);
+		horizontale.setStrokeWidth(1);
+		horizontale.setStroke(new Color(0,0,0,0.5));
+		
+		verticale.setStartY(53 + offsetY);
+        verticale.setEndY(53 + 446 + offsetY);
+        verticale.setStrokeWidth(1);
+        verticale.setStroke(new Color(0,0,0,0.5));
+		
+		if (! added){
+		     root.getChildren().addAll(lines);
+		     added = true;
+		}
+
+	}
+	
+	@FXML
+	 protected void OnMouseClickedFrame(MouseEvent event) {
+		
+		
+	}
+	
+	
+	@FXML
+	 protected void OnMouseMovedOverFrame(MouseEvent event) {
+		
+		double x = event.getSceneX();
+		double y = event.getSceneY();
+
+		verticale.setStartX(x);
+        verticale.setEndX(x);
+        		
+		horizontale.setStartY(y);
+		horizontale.setEndY(y);
+		
+
+	}
+
 
 	public Label getFrameNumber() {
 		return frameNumber;
@@ -84,6 +148,11 @@ public class VCGUIController {
 
 	public boolean isRun() {
 		return run;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
 	}
 
 	
